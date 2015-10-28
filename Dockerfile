14778550f407ffb66c30a2cbec0eed035bc3d0d1
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-10-28 14:34:21 +0100

    upgraded openjdk and fixed truststore

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM zalando/ubuntu:15.10-1
+FROM zalando/ubuntu:15.10-2
 MAINTAINER Zalando SE
 
 RUN apt-get update && apt-get install -y openjdk-8-jdk
 
 # Note: Zalando CA was automatically imported into Java trust store by Debian
+
+# currently, ubuntu does not properly generate truststore for the jdk
+# workaround:
+RUN update-ca-certificates -f
 
 ADD utils /java-utils
 ENV PATH ${PATH}:/${JAVA_HOME}/bin:/java-utils
--- build.sh
+++ build.sh
@@ -8,14 +8,15 @@ echo "Building $IMG .."
 docker build  --no-cache=true -t $IMG .
 
 SQUASH_PATH=$(which docker-squash)
+SQUASH_VERSION="0.2.0"
 
 if [ -z "$SQUASH_PATH" ]; then
     # install Docker Squash from https://github.com/jwilder/docker-squash
-    TGZ=docker-squash-linux-amd64-v0.1.0.tar.gz
-    wget https://github.com/jwilder/docker-squash/releases/download/v0.1.0/$TGZ
+    TGZ=docker-squash-linux-amd64-v${SQUASH_VERSION}.tar.gz
+    wget https://github.com/jwilder/docker-squash/releases/download/v${SQUASH_VERSION}/$TGZ
     sudo tar -C /usr/local/bin -xzvf $TGZ
     rm $TGZ
 fi
 
 echo 'Squashing the image to save space..'
-docker save $IMG | sudo docker-squash -t $IMG | docker load
+docker save $IMG | sudo docker-squash -verbose -t $IMG | docker load
