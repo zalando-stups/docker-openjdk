00ef3a135e4becd0f2b7e1e05179c0b25cd7242b
Henning Jacobs <henning@jacobs1.de>
2015-12-11 18:33:21 +0100

    use custom ELB without any instances to test SSL cert

--- test/catest/CATest.java
+++ test/catest/CATest.java
@@ -6,11 +6,19 @@ public final class CATest {
 
 	public static void main(final String[] args) {
 		try {
-			System.out.println("Testing SSL connection to https://maui-eagle.stups-test.zalan.do/signin ...");
-			final URL url = new URL("https://maui-eagle.stups-test.zalan.do/signin");
+			System.out.println("Testing SSL connection to https://ssl-test.stups-test.zalan.do/ ...");
+			final URL url = new URL("https://ssl-test.stups-test.zalan.do/");
 			url.openStream();
 			System.out.println("No problems fetching the page! SSL seems to work.");
 			System.exit(0);
+        } catch (final java.io.IOException e) {
+            if (e.getMessage().startsWith("Server returned HTTP response code: 503")) {
+                System.out.println("No problems with SSL cert (server returned 503)! SSL seems to work.");
+                System.exit(0);
+            } else {
+                e.printStackTrace();
+                System.exit(1);
+            }
 		} catch (final Exception e) {
 			e.printStackTrace();
 			System.exit(1);
